2bee21a29b9b26a2cd20ff9c24b15018be29de9a
RoelKneepkens <roelkneepkens@shipitsmarter.com>
2022-04-08 14:16:19 +0200

    Roels tests with some styles

--- src/extension.ts
+++ src/extension.ts
@@ -56,8 +56,12 @@ export function activate(context: vscode.ExtensionContext) {
   }
 
   function getCarrierDetail(): string {
-	let html2: string = ``;
-	html2 	+= ` 
+	let lanes = getLanes();
+	let htmlHeader: string = ``;
+	let htmlBody: string =``;
+	let html: string = ``;
+	let htmlFiles: string = ``;
+	htmlHeader 	+= ` 
 	<div class="header">
 		<h1>Details</h1>
 	</div>
@@ -67,6 +71,37 @@ export function activate(context: vscode.ExtensionContext) {
 		<a href="#specifications">Specifications</a>
 	</div>
 	`;
+	let files = vscode.workspace.findFiles('*.*').then(f =>{
+		f.forEach(function(filex){
+			htmlFiles += `<tr><td>${filex.path}</td></tr>`;
+		});
 
-	return html2;
+		
+		
+	});
+	
+	htmlBody += `<div class="detailpane">
+	<div style="overflow-x:auto;">
+		<table class="table">
+			<thead>
+			<tr><td>Lane</td><td>EXPRESS</td><td>ECONOMY</td></tr>
+			</thead>
+			<tbody>
+				<tr><td>NL-NL</td><td class="checkmark"><i class="fa fa-check"></i></td><td class="checkmark"><i class="fa fa-check"></i></td></tr>
+				<tr><td>NL-DE</td><td class="checkmark"><i class="fa fa-check"></i></td><td class="checkmark"><i class="fa fa-check"></i></td></tr>
+				<tr><td>NL-CH</td><td class="checkmark"><i class="fa fa-check"></i></td><td class="checkmark"><i class="fa fa-remove"></i></td></tr>
+				<tr><td>NL-US</td><td class="checkmark"><i class="fa fa-check"></i></td><td class="checkmark"><i class="fa fa-remove"></i></td></tr>
+				` + htmlFiles + `
+			</tbody>
+		</table>
+	</div>
+</div>`;
+
+	html += htmlHeader + htmlBody;
+	return html;
   }
+
+  function getLanes(): string[]{
+	return ['NL-NL','NL-DE','NL-CH', 'NL-US'];
+  }
+
